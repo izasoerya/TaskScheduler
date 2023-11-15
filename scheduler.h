f2f9b9524dd17bb8c09f1486782aec26bb20a32a
izasoerya <ihza.surya@gmail.com>
2023-11-15 17:42:55 +0700

    fix bugs

--- scheduler.h
+++ scheduler.h
@@ -1,23 +1,23 @@
-#ifndef SCHEDULELR_H
-#define SCHEDULELR_H
+#ifndef TASKSCHEDULER_H
+#define TASKSCHEDULER_H
 
 #include <Arduino.h>
+typedef void (*TaskFunction)();  // Define a typedef for the function pointer type
 
-class TaskScheduler{
+class TaskScheduler {
 private:
-    typedef void (*TaskFunction)();  // Define a typedef for the function pointer type
-    char* name;
-    uint16_t delay;
-    uint8_t idTask;
+    String taskName;
+    uint16_t taskDelay;
+    uint8_t taskId;
     uint32_t prevMillis = 0;
+    bool isSuspended = false;
     TaskFunction taskFunc;
-    bool isSuspended = false;  
 
 public:
-    TaskScheduler(uint8_t idTask, char* name, uint16_t delay, TaskFunction taskFunc);
+    TaskScheduler(uint8_t userId, String userName, uint16_t userDelay, TaskFunction userTask);
     void runTask();
     void suspendTask();
     void resumeTask();
 };
 
-#endif
+#endif // TASKSCHEDULER_H
