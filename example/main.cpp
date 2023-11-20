33080346bf3df8666dbfbf11172ee6f3a7a74a73
izasoerya <ihza.surya@gmail.com>
2023-11-20 14:32:36 +0700

    feat: add example

---
+++ example/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "scheduler.h"
+
+void doTask1() {
+    Serial.println("Task 1");
+}
+void doTask2() {
+    Serial.println("Task 2");
+}
+
+TaskScheduler task1(1, "TASK1", 1000, doTask1);
+TaskScheduler task2(1, "TASK2", 1000, doTask2);
+
+void main() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    task1.runTask();
+}
